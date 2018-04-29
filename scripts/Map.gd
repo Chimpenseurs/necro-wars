5dcbda2d9e07d2f25c6c4021983d0f0a755d0cb9
Avzgui <contact.avzgui@gmail.com>
2018-04-29 17:03:00 +0200

    init character deplacement

--- scripts/Map.gd
+++ scripts/Map.gd
@@ -4,6 +4,7 @@ const GRID_SIZE = 512 # in pixel
 const CURSOR_SIZE = 32
 
 var cursor_on_character = null
+var character_selected = null
 
 func _init():
 	self.set_process_input(true)
@@ -20,10 +21,23 @@ func _input(event):
 		
 		if event.is_pressed() :
 			
+			var cursor_pos = get_node("cursor").position
+			
+			# CHARACTER MOTION
+			if event.is_action("ui_select") :
+				if character_selected == null :
+					if cursor_on_character != null :
+						character_selected = cursor_on_character
+						character_selected.show_move_zone()
+				elif cursor_on_character == null :
+					# must be changed to a function
+					character_selected.position = cursor_pos
+					character_selected.hide_move_zone()
+					character_selected = null
+			
+			# CURSOR MOTION
 			if event.is_action("ui_right") or event.is_action("ui_left") or event.is_action("ui_up") or event.is_action("ui_down") :
 				
-				# CURSOR MOTION
-				var cursor_pos = get_node("cursor").position
 				if event.is_action("ui_right") :
 					cursor_pos.x += CURSOR_SIZE
 				elif event.is_action("ui_left") :
@@ -33,20 +47,24 @@ func _input(event):
 					cursor_pos.y -= CURSOR_SIZE
 				elif event.is_action("ui_down") :
 					cursor_pos.y += CURSOR_SIZE
+			
+			# check if cursor in on something after action
+			if is_in_grid(cursor_pos) :
 				
-				# if movement is valid
-				if is_in_grid(cursor_pos) :
-					# change cursor position
-					get_node("cursor").position = cursor_pos
-					
-					# if cursor was on a character
-					if cursor_on_character != null :
+				# change cursor position
+				get_node("cursor").position = cursor_pos
+				
+				
+				# if cursor was on a character
+				if cursor_on_character != null :
+					if character_selected == null :
 						cursor_on_character.hide_move_zone()
-						cursor_on_character = null
-					
-					# if cursor is on a character
-					if cursor_pos == get_node("character").position :
-						cursor_on_character = get_node("character")
+					cursor_on_character = null
+				
+				# if cursor is on a character
+				if cursor_pos == get_node("character").position :
+					cursor_on_character = get_node("character")
+					if character_selected == null :
 						cursor_on_character.show_move_zone()
 
 
