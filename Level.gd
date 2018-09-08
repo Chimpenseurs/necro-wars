61e4831bb246ab5c307838d67ea75339af5b8d3d
Adrien FAURE <adrien.faure2@gmail.com>
2018-09-08 19:20:05 +0200

    oloy

--- Level.gd
+++ Level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const Dcell = preload("./common/scripts/Dijkstra.gd")
+
 var units = []
 var level_configuration = { "tile_size" : 32 }
 
@@ -27,42 +29,37 @@ func display_zone(zone):
 func clear_zones():
 	zones.clear()
 	
-class Dcell:
-	var pos
-	var distance
-
-	func _init(p, d):
-		self.pos = p
-		self.distance = d
-
 # The pos is in map unit
-func dijkstra(pos, distance):
+func dijkstra(pos, distance_max):
 	var cell = map.get_cellv(pos)
-	if cell == map.INVALID_CELL:
+	if cell == null:
 		#Initial cell does not exist
 		return null
 
-	var stack = [ Dcell.new(pos, 0) ]
+	var initial = Dcell.Dcell.new(cell, 0)
+	var stack = [ initial ]
 	var memory = { }
-	memory[stack[0].pos] = stack[0]
+	
+	memory[ initial.cell.pos ] = initial
 
 	while stack.size() > 0:
-		print(stack.size())
+
 		var current = find_min_in_array(stack)
-		var neighbors = [
-			Dcell.new(current.pos+Vector2(1, 0), -1),
-			Dcell.new(current.pos+Vector2(0, 1), -1), 
-			Dcell.new(current.pos+Vector2(-1, 0), -1), 
-			Dcell.new(current.pos+Vector2(0, -1), -1) 
-		]
+		var neighbors = map.get_neighbors(current.cell.pos)
+
 		for neighbor in neighbors:
-			if map.get_cellv(neighbor.pos) != map.INVALID_CELL:
-				if !memory.has(neighbor.pos):
-					memory[neighbor.pos] = neighbor
-				if memory[neighbor.pos].distance == -1 or current.distance + 1 < memory[neighbor.pos].distance :
-					neighbor.distance = current.distance + 1
-					if neighbor.distance < distance:
-						stack.append(neighbor)
+			var dcell = Dcell.Dcell.new(neighbor, -1)
+	
+			var alt = current.distance + current.cell.speed
+
+			if alt  <= distance_max:
+				if !memory.has(dcell.cell.pos):
+					memory[dcell.cell.pos] = dcell
+
+				if memory[dcell.cell.pos].distance == -1 or alt < memory[dcell.cell.pos].distance:
+					dcell.distance = alt
+					stack.append(dcell)
+
 	return memory
 
 func find_min_in_array(array):
--- map/scripts/Map.gd
+++ map/scripts/Map.gd
@@ -1,12 +1,39 @@
-extends TileMap
+extends Node2D
 
+const Dcell = preload("res://common/scripts/Dijkstra.gd")
 
+onready var layer0 = get_node("Layer0")
 onready var layer1 = get_node("Layer1")
 
 # Information zones
 onready var zones = get_node("Zones")
 
 func _ready():
-	# Gently reminder
-	print(self.map_to_world(Vector2(1, 0)))
-	print(self.world_to_map(Vector2(32, 0)))
+	pass
+	
+func get_cellv(pos):
+	var layer_cell = layer0.get_cellv(pos)
+	if layer_cell != layer0.INVALID_CELL:
+		if layer_cell == 468:
+			return Dcell.Cell.new(pos, 2)
+		else:
+			return Dcell.Cell.new(pos, 1)
+	return null
+
+func is_valid_cellv(pos):
+	return layer0.get_cellv(pos) != layer0.INVALID_CELL
+
+func get_neighbors(pos):
+	var neighbor_poses = [ 
+		pos+Vector2(1, 0), 
+		pos+Vector2(0, 1), 
+		pos+Vector2(-1, 0), 
+		pos+Vector2(0, -1) 
+	]
+
+	var neighbors = []
+	for n in neighbor_poses:
+		var layer_cell = self.get_cellv(n)
+		if layer_cell != null:
+			neighbors.append(layer_cell)
+	return neighbors
