5c2915888e0fb9c6a74495ce9344bbab31985bea
Avzgui <contact.avzgui@gmail.com>
2018-09-09 16:24:31 +0200

    godot does not move each delta anymore

--- units/scripts/Godot.gd
+++ units/scripts/Godot.gd
@@ -1,19 +1,17 @@
 extends Node2D
 
-var pos = Vector2(2, 0)
+var pos = Vector2(0, 0)
 var speed = 3
 
 func _ready():
-	pass
-
-func _process(delta):
-	self.position = pos * 32
+	pos = self.position / 32
 
 func get_pos():
 	return pos
 
 func move(pos):
 	self.pos = pos
+	self.position = pos * 32
 
 func get_movement_zone():
 	
