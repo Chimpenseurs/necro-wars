22ec5be1f3ef6401f949df96439afdad2ebc77c5
Avzgui <contact.avzgui@gmail.com>
2018-09-09 18:09:43 +0200

    add sprite into zone tileset

--- units/scripts/Unit.gd
+++ units/scripts/Unit.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 var pos = Vector2(0, 0)
-var speed = 3
+var speed = 3  # TODO : separate speed and move range
+var attack_range = 1
 
 func _ready():
 	self.pos = self.position /32
